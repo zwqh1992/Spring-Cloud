3c619e1dcd2132ced8a46e1169d394f398bb9100
朝雾轻寒 <466595865@qq.com>
2019-12-07 09:47:14 +0800

    eureka-server-b

---
+++ spring-cloud-eureka/spring-cloud-eureka-server-b/src/test/java/cn/zwqh/springcloud/SpringCloudEurekaServerBApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zwqh.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
